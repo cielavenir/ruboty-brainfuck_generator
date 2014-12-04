231dee1282159298751b6f646263a87c9f267230
cielavenir <cielartisan@gmail.com>
2014-12-04 18:22:28 +0900

    fix

--- lib/ruboty/handlers/brainfuck_generator.rb
+++ lib/ruboty/handlers/brainfuck_generator.rb
@@ -3,7 +3,7 @@ require "ruboty/brainfuck_generator/actions/generate"
 module Ruboty
   module Handlers
     class BrainfuckGenerator < Base
-      on /brainfuck (?<text>.*?)/, name: 'brainfuck', description: 'generate brainfuck code'
+      on /brainfuck (?<text>.*)/, name: 'brainfuck', description: 'generate brainfuck code'
 
       def brainfuck(message)
         Ruboty::BrainfuckGenerator::Actions::Generate.new(message).call
